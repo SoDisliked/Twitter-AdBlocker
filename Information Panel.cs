fdd530cdc23837c22863b9c0b195249c9c90f5f3
SoDisliked <90557319+SoDisliked@users.noreply.github.com>
2022-08-27 14:59:39 +0300

    Add files via upload

---
+++ Information Panel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterAdBlocker
+{
+    public static class AdBlocker
+    {
+        public static Version = "0.1.0"
+        public static Company = "SoDisliked"
+        public static Product = "TwitterAdBlocker"
+        public static DefaultBlockPage = "Chrome
+    }
+}
