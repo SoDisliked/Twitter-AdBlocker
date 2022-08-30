2b1e4d00eeae0787dff7fca1baccd70aa70a12e5
SoDisliked <90557319+SoDisliked@users.noreply.github.com>
2022-08-30 11:47:12 +0300

    Add files via upload

---
+++ Extension Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace TwitterAdBlocker.Extensions
+{
+    internal static class TypeExtensions
+    {
+        public static ConstructorInfo? GetConstructor(this Type type, BindingFlags bindingFlags, Type[] types)
+        {
+            return type.GetConstructor(bindingFlags, null, types, null);
+        }
+    }
+}
